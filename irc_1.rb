1e249f5cd0716e6910e30fc603eafe839390a3bc
lsavoie <twosavoie@gmail.com>
2018-08-08 12:49:41 -0500

    one side of irc

--- irc_1.rb
+++ irc_1.rb
@@ -2,24 +2,24 @@
 #I don't know if this branch thing is working or not
 require 'socket'
 
-server = 'irc.freenode.net'
+server = 'moon.freenode.net'
 port = 6667
 socket = TCPSocket.open(server, port)
 
 nickname = 'SkillcrushBotOMG'
+channel = '#twos22'
 
 socket.puts "NICK #{nickname}"
 socket.puts "USER #{nickname} O * #{nickname}"
+socket.puts "JOIN #{channel}"
+socket.puts "PRIVMSG #{channel} :This Works!"
 
 while message = socket.gets do
   puts message
 
   if message.match('^PING :')
-
     server = message.split(':').last
-
     puts "PONG #{server}"
-
     socket.puts "PONG #{server}"
   end
 end
